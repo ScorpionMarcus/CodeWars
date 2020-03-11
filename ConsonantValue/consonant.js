081ac3c79174beae0091537faf21d74a8649963b
Marcus LeGault <marcus.legault@scorpion.co>
2020-03-11 16:34:40 -0700

    consonant value

--- ConsonantValue/consonant.js
+++ ConsonantValue/consonant.js
@@ -2,7 +2,7 @@ function solve(s) {
 
     // let alpha = 'abcdefghijklmnopqrstuvwxyz';
     // replace each vowel in s with a blank space, then remove redundancies
-    let arr = s.slice().replace(/[aeiou]/gi, ' ').replace(/\s+/g, ' ')
+    let arr = s.slice().replace(/[aeiou]/g, ' ').replace(/\s+/g, ' ')
     console.log(arr);
 
     // separate into items in an array
@@ -11,25 +11,20 @@ function solve(s) {
     console.log(arr);
 
     // split each item into separate arrays
+    // assign each letter in s to a number using charCodeAt
+    // use reduce so that the numbers in each array are added together ie a,b becomes a+b starting at the first position 0
 
-    arr = arr.map(v => v.split(''));
+    arr = arr.map(v => v.split('').map(v => v.charCodeAt() - 96).reduce((a, b) => a + b, 0));
     console.log(arr);
 
-    arr = arr.map(v => v.charCodeAt() - 96);
-    console.log(arr);
+    // return the highest number item in the array
+    // ...spread operator (all the numbers in arr)
 
-    return 0;
+    return Math.max(...arr);
 };
 
-solve("mischtschenkoana");
+console.log(solve("mischtschenkoana"));
 
 
 
-//'b'.charCodeAt(0) - 96
-
-// function solve(s) {
-//     s=s.toLowerCase();
-//     let arr=s.slice().replace(/[aeiuo]/g,' ').replace(/\s+/g,' ');
-//     arr=arr.split(' ').map(v=>v.split('').map(v=>v.charCodeAt()-96).reduce((a,b)=>a+b,0))
-//     return Math.max(...arr);
-// };
+// console.log('b'.charCodeAt(0) - 96);
